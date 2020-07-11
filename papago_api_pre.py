c682f00ef966925f8259422921ee4cff3c38021a
hojune-choi <hojune.choi.me@gmail.com>
2020-07-11 21:08:13 +0900

    add def translate

--- papago_api_pre.py
+++ papago_api_pre.py
@@ -9,20 +9,46 @@ papago_nmt_secret = ""
 papago_detect_id = ""
 papago_detect_secret = ""
 
-#언어 확인
+#언어 감지
 def detect_language(txt):
-    encText = urllib.parse.quote(txt)
-    data = "text=" + encText
+    encQuery = urllib.parse.quote(txt)
+    data = "query=" + encQuery
 
-    url = "https://openapi.naver.com/v1/papago/n2mt"
+    url = "https://openapi.naver.com/v1/papago/detectLangs"
 
     request = urllib.request.Request(url)
     request.add_header("X-Naver-Client-Id",papago_detect_id)
     request.add_header("X-Naver-Client-Secret",papago_detect_secret)
     response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    
     rescode = response.getcode()
     if(rescode==200):
         response_body = response.read()
         print(response_body.decode("utf-8"))
     else:
         print("Error Code:" + rescode)
+
+#번역
+def translate(txt):
+    encText = urllib.parse.quote(txt)
+    data = "source=en&target=ko&text=" + encText
+    
+    url = "https://openapi.naver.com/v1/papago/n2mt"
+    
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",papago_nmt_id)
+    request.add_header("X-Naver-Client-Secret",papago_nmt_secret)
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+    else:
+        print("Error Code:" + rescode)
+
+
+if __name__ == "__main__":
+    txt = "HELLO! This is for ppt translation."
+    lang = detect_language(txt)
+    print(translate(txt))
