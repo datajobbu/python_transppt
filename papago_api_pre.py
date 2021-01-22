be8cfe03e061574239123d2e92be8b562bebda07
hojuly <hojune.choi.me@gmail.com>
2021-01-22 14:13:08 +0900

    add lang type

--- papago_api_pre.py
+++ papago_api_pre.py
@@ -3,44 +3,13 @@ import ssl
 import json
 import urllib.request
 
-import os
-import ssl
-import json
-import urllib.request
-
-
-def detect_language(txt, dtl_id, dtl_pw):
-    """언어감지"""
-    papago_detect_id = dtl_id
-    papago_detect_secret = dtl_pw
-    encQuery = urllib.parse.quote(txt)
-    data = "query=" + encQuery
-
-    url = "https://openapi.naver.com/v1/papago/detectLangs"
-
-    request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id",papago_detect_id)
-    request.add_header("X-Naver-Client-Secret",papago_detect_secret)
-    res_ssl = ssl._create_unverified_context()
-    response = urllib.request.urlopen(request, data=data.encode("utf-8"), context=res_ssl)
-    
-    rescode = response.getcode()
-
-    if(rescode==200):
-        response_body = response.read()
-        
-        res = json.loads(response_body.decode('utf-8'))
-        return(res['langCode'])
-
-    else:
-        print("Error Code:" + rescode)
-
 
 def translate(txt, tp, nmt_id, nmt_pw):
     """번역"""
     if tp == "0":
         src = "en"
         tgt = "ko"
+
     elif tp == "1":
         src = "ko"
         tgt = "en"
@@ -65,8 +34,3 @@ def translate(txt, tp, nmt_id, nmt_pw):
 
     else:
         return("Error Code:" + rescode)
-
-
-if __name__ == "__main__":
-    txt = "안녕 July?"
-    detect_language(txt)
